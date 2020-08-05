8a123b19f4c296d342a4c6d7e944812a064545da
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-08-06 09:48:05 +1200

    config created
    created config file and added it to index

---
+++ site_template/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+ini_set('display_errors', 'On'); // change to 'Off' when live
+
+define ("DB_NAME","sampangk71280");
+define ("DB_USERNAME","_sampangk71280");
+define ("DB_PASSWORD","d6de441b9d3bb5ae87f878849e2e0904");
+define ("DB_HOST","localhost");
+
+?>
