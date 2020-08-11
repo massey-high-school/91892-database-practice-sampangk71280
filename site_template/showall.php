4713c2594f83afa7323077afd261c5f6ca9cc94d
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-08-11 12:05:08 +1200

    results pt 1

---
+++ site_template/showall.php
@@ -0,0 +1,58 @@
+     <?php include("topbit.php");
+
+        $find_sql = "SELECT *
+FROM `L2_DB_Prac_game_details`";
+        $find_query = mysqli_query($dbconnect, $find_sql);
+        $find_rs = mysqli_fetch_assoc($find_query);
+        $count = mysqli_num_rows($find_query);
+
+?>
+
+        <div class="box main">
+            <h2>All Results</h2>
+            
+            
+            <?php
+            
+            if($count <1 ) {
+                
+                ?>
+            
+            <div class="error">
+            
+                Sorry! There are no results that match your search.
+                Please use the search box int he side bar to try again.
+            
+            </div>      <!-- end error -->
+            
+            <?php 
+            } // end no results if
+            
+            else {
+                do
+                {
+                    ?>
+            
+            <!-- Results go here -->
+            <div class="results">
+                You have results!
+            </div>
+            
+            <?php
+                    
+                    
+                } // end results 'do'
+                
+            
+                while
+                    ($find_rs=mysqli_fetch_assoc($find_query));
+                
+            } // end else
+                     
+            ?>
+            
+
+            
+        </div> <!-- / main -->
+        
+    <?php include("bottombit.php") ?>
