97d02e2e9c7ba1852064d929018e0a9d19ab607d
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-09-03 09:21:58 +1200

    basic search part 1

--- site_template/bottombit.php
+++ site_template/bottombit.php
@@ -3,7 +3,18 @@
            <h2>Add an App |
                <a class="side" href="showall.php">Show All</a></h2>
            
-           <p>This is where the search area goes
+           <form class="searchform" method="post"
+            action="name_dev.php" enctype="multipart/form-data">
+            
+               <input class="search" type="text" name="dev_name"
+                size="32" value="" required placeholder="App Name / Developer Name..."/> 
+               
+               <input class="submit" type="submit"
+                name="find_game_name" value="&#xf002;"/>
+               
+               
+               
+            </form>
 
             
         </div> <!-- / side bar -->
---
+++ site_template/results.php
@@ -0,0 +1,147 @@
+<?php
+            
+            if($count <1 ) {
+                
+                ?>
+            
+            <div class="error">
+            
+                Sorry! There are no results that match your search.
+                Please use the search box int he side bar to try again.
+            
+            </div>      <!-- end error -->
+            
+            <?php 
+            } // end no results if
+            
+            else {
+                do
+                {
+                    ?>
+            
+            <!-- Results go here -->
+            <div class="results">
+                
+                <!-- Heading and subtitle -->
+                
+                <div class="flex-container"> 
+                    <div>
+                        <span class="sub_heading">
+                            <a href="<?php echo $find_rs['URL']; ?>">
+                            <?php echo $find_rs['Name']; ?>
+                            </a>
+                        </span>
+                    </div> <!-- / Title -->
+                
+                    <?php
+                        if($find_rs['Subtitle'] != "")
+                        
+                        {
+                        
+                        ?>
+                    
+                    <div>
+                        &nbsp;&nbsp| &nbsp;&nbsp;
+                        <?php echo $find_rs['Subtitle'] ?> 
+                        
+                    </div> <!-- / subtitle -->
+                    
+                    
+                    <?php
+                            
+                        }
+                    
+                    ?>
+                    
+                    
+                </div>
+                <!-- / Heading and subtitle -->
+                
+                <!-- Ratings Area-->
+                
+                <div class="flex-container">
+                
+                        <!-- Partial Stars Original Source:
+                        https://codepen.io/Bluetidepro/pen/GkpEa -->
+                        <div class="star-ratings-sprite">
+                            <span style="width:<?php echo $find_rs['User Rating'] / 5 * 100; ?>%" class="star-ratings-sprite-rating"></span> 
+                    
+                        </div> <!-- / star rating div -->
+                    
+                        <div class="actual-rating">
+                            (<?php echo $find_rs['User Rating'] ?> rating based on <?php echo number_format ($find_rs['Rating Count']) ?> ratings)
+                        </div> <!-- / text rating div -->
+                    
+                    
+
+                </div> <!-- / ratings flexbox -->
+                
+                <!-- / Ratings Area -->
+                
+                <!-- Price -->
+                
+                <?php 
+                    
+                    if($find_rs['Price'] == 0 ){
+                        ?>
+                
+                    <p>
+                        Free 
+                        <?php 
+                            if ($find_rs['In App'] == 1) 
+                            {
+                                ?>
+                                    (In App Purchase)
+                                <?php
+                                
+                                
+                            } // end In App if
+                        ?>
+                        
+                
+                    </p>
+                
+                    <?php
+                    } // end price if
+                    
+                    else {
+                        
+                        ?>
+                    <b>Price</b>: $<?php echo $find_rs['Price'] ?>
+                
+                    <?php
+                        
+                    } // end price else (displays cist)
+                    
+                ?>
+                
+                <!--- / Price --->
+                
+                <p>
+                    <!-- Developer, Genre and Age... -->
+                    <b>Developer:</b><?php echo $find_rs['DevName'] ?><br/>
+                    <b>Genre:</b><?php echo $find_rs['Genre'] ?><br/>
+                    Suitable for ages <b><?php echo $find_rs['Age']?> </b> and up
+                </p>
+                
+                <p>
+                    <i><?php echo $find_rs['Description'] ?> </i>
+                    
+                </p>
+                
+            </div> <!-- / results -->
+            
+            <br />
+        
+            <?php
+                    
+                    
+                } // end results 'do'
+                
+            
+                while
+                    ($find_rs=mysqli_fetch_assoc($find_query));
+                
+            } // end else
+                     
+            ?>
