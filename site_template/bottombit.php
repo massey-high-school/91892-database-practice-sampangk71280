2f164b09115c3f048737e6bf6c42b4a27b26d1ef
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-09-08 12:15:04 +1200

    advanced search part 1 completed

--- site_template/bottombit.php
+++ site_template/bottombit.php
@@ -11,8 +11,6 @@
                
                <input class="submit" type="submit" name="find_dev_name" value="&#xf002;"/>
                
-               
-               
             </form>
             
             <form class="searchform" method="post" action="free.php"
@@ -22,6 +20,39 @@
             
             </form>
 
+            <br />
+            <hr />
+            <hr />
+            
+            <div class="advanced-frame">
+            
+            <h2>Advanced Search</h2>
+            
+                <form class="searchform" method="post"
+                    action="advanced.php" enctype="multipart/form-data">
+                
+                <input class="adv" type="text" name="app_name" size="40" value="" placeholder="App Name / Title..."/>
+                    
+                <input class="adv" type="text" name="dev_name" size="40" value="" placeholder="Developer..."/>
+                    
+                <input class="submit advanced-button" type="submit"
+                name="advanced" value="Search &nbsp; &#xf002;" />
+                
+                <!-- Genre Dropdown -->
+                <select name="genre">
+                <!--- get options from database -->
+                    <?php
+                    
+                    
+                    ?>
+                    
+                    
+                </select>
+                
+                </form>
+           
+            </div> <!-- / advanced frame -->
+            
             
         </div> <!-- / side bar -->
         
