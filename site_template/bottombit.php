bf9711b2e83007df2db9ebd5efd7211021cff97a
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-09-10 09:52:49 +1200

    advanced search pt 2 incomplete

--- site_template/bottombit.php
+++ site_template/bottombit.php
@@ -35,19 +35,35 @@
                     
                 <input class="adv" type="text" name="dev_name" size="40" value="" placeholder="Developer..."/>
                     
-                <input class="submit advanced-button" type="submit"
-                name="advanced" value="Search &nbsp; &#xf002;" />
+
                 
                 <!-- Genre Dropdown -->
-                <select name="genre">
+                <select class="search adv" name="genre">
                 <!--- get options from database -->
                     <?php
+                        $genre_sql="SELECT *
+                                    FROM `L2_DB_Prac_genre`
+                                    ORDER BY `L2_DB_Prac_genre`.`Genre` ASC";
+                        $genre_query=mysqli_query($dbconnect, $genre_sql);
+                        $genre_rs=mysqli_fetch_assoc($genre_query);
                     
+                        do {
+                            ?>
+                        <option value="<?php echo $genre_rs['Genre']; ?>"><?php echo $genre_rs['Genre']; ?></option>
+                            
+                        <?php
+                            
+                        }   // end genre do loop
+                    
+                        while ($genre_rs=mysqli_fetch_assoc($genre_query)) 
                     
                     ?>
                     
                     
                 </select>
+                    
+                <input class="submit advanced-button" type="submit"
+                name="advanced" value="Search &nbsp; &#xf002;" />
                 
                 </form>
            
@@ -60,8 +76,4 @@
             CC Kristelle Sampang 2020
         </div> <!-- / footer -->
                 
-        
-    </div> <!-- / wrapper -->
     
-            
-</body>
