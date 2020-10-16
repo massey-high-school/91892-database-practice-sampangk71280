333760abd1946367a049635ca14fc0a036cbaf22
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2020-10-16 14:22:30 +1300

    entry form pt1 completed

--- site_template/addentry.php
+++ site_template/addentry.php
@@ -1,10 +1,26 @@
-     <?php include("topbit.php") ?>
+     <?php include("topbit.php");
+
+// Initialise from variables 
+
+$app_name = "";
+$subtitle = "";
+$url = "";
+$dev_name = "";
+$age = "";
+$rating = "";
+$rate_count = "";
+$cost = "";
+$description = "";
+
+$has_errors = "no";
+
+?>
 
         <div class="box main">
             <div class="add-entry">
             <h2>Add an Entry</h2>
                 
-            <form method="post" enctype="multipart/form-data" action="">
+            <form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF ']); ?>">
             
             </form>    
 
